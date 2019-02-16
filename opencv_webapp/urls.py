c3948c912cb39ee282f5de1164ea9db1c77b4687
teguhgunaw <teguhgunawan21@gmail.com>
2019-02-16 23:30:27 +0700

    detection face

--- opencv_webapp/urls.py
+++ opencv_webapp/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static  # add
 urlpatterns = [
     url(r'^$', views.first_view, name='first_view'),
     url(r'^uimage/$', views.uimage, name='uimage'),  # add
+    url(r'^dface/$', views.dface, name='dface'),  # add
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # add
